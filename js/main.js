a283ad0f8ffe74125d44b45de74d55f793910cf1
Julius Mattsson <julius@aino.se>
2015-10-21 17:04:02 +0200

    tabs, just add 'tabbed' class to module

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 $( document ).ready(function() {
+
+
+  // ======================================================================================
+  //  Change List Filters
+  // ======================================================================================
+  $('#changelist-filter select').each(function(i) {
+    if ( !$(this).find('option').first().is(':selected') ) {
+      $(this).parent().addClass('active')
+    }
+  })
+
+  // ======================================================================================
+  //  Change List Actions
+  // ======================================================================================
   var $changelistForm = $('#changelist-form')
   var $changelistFormCheckboxes = $changelistForm.find('input[type=checkbox]')
 
@@ -9,4 +23,42 @@ $( document ).ready(function() {
       $changelistForm.find('.actions').slideUp(100)
     }
   })
+
+
+  // ======================================================================================
+  //  Tabbed
+  // ======================================================================================
+  var $tabbedModules = $('.module.tabbed')
+  var tabbedModulesLength = $tabbedModules.length
+  var $tabs = $('<ul>')
+
+
+  if ( tabbedModulesLength > 0 ) {
+    var tabbedIndex = 0
+    var h1 = $('#content h1').first().text()
+
+    // Create tab ul
+    $('fieldset.module').first().before($tabs)
+    $tabs.addClass('tabs').append('<li data-target="untabbed" class="active">'+ h1 + '</li>')
+
+    $('fieldset.module').each(function(i) {
+      var thisTitle = $(this).find('h2').first().text()
+      if ( $(this).hasClass('tabbed') ) {
+        tabbedIndex++
+        $(this).addClass('tab-'+tabbedIndex)
+        $tabs.append('<li data-target="tab-'+tabbedIndex+'">'+thisTitle+'</li>')
+      } else {
+        $(this).addClass('untabbed')
+      }
+    })
+  }
+
+  // Events
+  $tabs.find('li').on('click', function(e) {
+    $tabs.find('li').removeClass('active')
+    $(this).addClass('active')
+    $('fieldset.module').hide()
+    $('.'+$(this).data('target')).show()
+  })
+
 });
