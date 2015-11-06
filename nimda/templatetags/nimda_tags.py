7cc79c5ffa9f4552cfd02f4ec83c3624b2199096
Mikko Hellsing <mikko@aino.se>
2015-11-06 03:55:54 +0100

    use index data

--- nimda/templatetags/nimda_tags.py
+++ nimda/templatetags/nimda_tags.py
@@ -2,8 +2,7 @@ import random
 from django import template
 from nimda.base import NimdaException
 from django.core.urlresolvers import reverse
-from operator import itemgetter
-
+from django.apps import apps
 
 register = template.Library()
 
@@ -26,22 +25,20 @@ def sidebar_menu(context):
 
 @register.inclusion_tag('admin/includes/model_summary.html', takes_context=True)
 def model_summary(context):
-    # big fat disclaimer, this will only work with default site
-    from django.contrib.admin.sites import site
-    colors = ['aqua', 'green', 'red', 'white', 'black']
-    random.seed('nimdalhc')
-    random.shuffle(colors)
-    app_labels = []
-    models = []
-    for m in site._registry.keys():
-        if m._meta.app_label not in app_labels:
-            app_labels.append(m._meta.app_label)
-        cidx = app_labels.index(m._meta.app_label) % len(colors)
-        models.append({
-            'name': m._meta.verbose_name_plural,
-            'app': m._meta.app_label,
-            'count': m._default_manager.all().count(),
-            'color': colors[cidx],
-            'url': reverse('admin:%s_%s_changelist' % (m._meta.app_label, m._meta.model_name))
-        })
-    return {'models': sorted(models, key=itemgetter('app', 'name'))}
+    if 'app_list' in context:
+        colors = ['aqua', 'green', 'red', 'white', 'black']
+        random.seed('nimdalhc')
+        random.shuffle(colors)
+        models = []
+        for j, app in enumerate(context['app_list']):
+            for m in app['models']:
+                cidx = j % len(colors)
+                M = apps.get_model(app['app_label'], m['object_name'])
+                models.append({
+                    'label': app['app_label'][0].upper(),
+                    'name': m['name'],
+                    'count': M._default_manager.all().count(),
+                    'color': colors[cidx],
+                    'url': m['admin_url']
+                })
+        return {'models': models}
