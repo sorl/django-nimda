70d833c7e469d073f659f84be8d99b69901fea69
Mikko Hellsing <mikko@aino.se>
2015-11-06 01:25:40 +0100

    adding pagination

--- nimda/base.py
+++ nimda/base.py
@@ -1,2 +1,13 @@
 class NimdaException(Exception):
     pass
+
+
+class NimdaSiteMixin(object):
+    def each_context(self, request):
+        """
+        Adds available_apps to each context
+        """
+        c = super(NimdaSiteMixin, self).each_context(request)
+        res = self.index(request)
+        c['available_apps'] = res.context_data['app_list']
+        return c
