6d1ac2218a791a221e7ba6f25186a99e72101aa8
Mikko Hellsing <mikko@aino.se>
2015-11-06 02:25:00 +0100

    add some fun on index

--- nimda/templatetags/nimda_tags.py
+++ nimda/templatetags/nimda_tags.py
@@ -1,5 +1,8 @@
+import random
 from django import template
 from nimda.base import NimdaException
+from django.core.urlresolvers import reverse
+from operator import itemgetter
 
 
 register = template.Library()
@@ -19,3 +22,26 @@ def sidebar_menu(context):
     else:
         raise NimdaException('Cannot find app list.')
     return {'available_apps': available_apps}
+
+
+@register.inclusion_tag('admin/includes/model_summary.html', takes_context=True)
+def model_summary(context):
+    # big fat disclaimer, this will only work with default site
+    from django.contrib.admin.sites import site
+    colors = ['aqua', 'green', 'red', 'white', 'black']
+    random.seed('nimdalhc')
+    random.shuffle(colors)
+    app_labels = []
+    models = []
+    for m in site._registry.keys():
+        if m._meta.app_label not in app_labels:
+            app_labels.append(m._meta.app_label)
+        cidx = app_labels.index(m._meta.app_label) % len(colors)
+        models.append({
+            'name': m._meta.verbose_name_plural,
+            'app': m._meta.app_label,
+            'count': m._default_manager.all().count(),
+            'color': colors[cidx],
+            'url': reverse('admin:%s_%s_changelist' % (m._meta.app_label, m._meta.model_name))
+        })
+    return {'models': sorted(models, key=itemgetter('app', 'name'))}
